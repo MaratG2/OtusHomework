a8b4869a7760535b24deb8cf261d0c6caf96a794
Marat <maratg2develop@gmail.com>
2023-07-10 17:36:24 +0400

    New plugins

---
+++ Assets/Plugins/MaratG2/CanvasScalerMatchChanger.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MaratG2
+{
+    [RequireComponent(typeof(CanvasScaler))]
+    public class CanvasScalerMatchChanger : MonoBehaviour
+    {
+        private CanvasScaler _canvasScaler;
+        private readonly float _defaultRatio = 16 / 9f - Mathf.Epsilon;
+        private float _deviceRatio = 16 / 9f;
+
+        private void Awake()
+        {
+            _canvasScaler = GetComponent<CanvasScaler>();
+        }
+
+        private void Update()
+        {
+            _deviceRatio = (float)Screen.width / Screen.height;
+            if (_deviceRatio >= _defaultRatio)
+                _canvasScaler.matchWidthOrHeight = 1f;
+            else
+                _canvasScaler.matchWidthOrHeight = 0f;
+        }
+    }
+}
